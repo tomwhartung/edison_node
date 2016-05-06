dae4362fc4963b228ddfd251b89d7f46a0a4fb79
Tom W Hartung <tomwhartung@gmail.com>
2016-05-06 02:38:56 +0000

    Complete version of index.js , we get an error but it's a start.

--- my_examples/01.Basics/index.js
+++ my_examples/01.Basics/index.js
@@ -1,11 +1,31 @@
 //
 // From the book "Node.js for Embedded Systems" by Patrick Mulder
+//    Chapter 2, Blink With JavaScript - Programming the Thing
 //
 var firmata = require( 'firmata' );
+var modem = '/dev/cu.usbserial-DJ008CEA';
 
+//
 // Main part
+//
 var board = new firmata.Board( modem, function(err) {
-   console.log( 'connected: ' + modem);
-   var ledOn = true;
+	console.log( 'connected: ' + modem);
+	var ledOn = true;
 	//
+	// Configure pin 13 as output
+	//
+	board.pinMode(13, board.MODES.OUTPUT);
+	//
+	// Blink the LED
+	//
+	setInterval(function() {
+		if (ledOn) {
+			console.log('ON');
+			board.digitalWrite(13, board.HIGH);
+		} else {
+			console.log('OFF');
+			board.digitalWrite(13, board.LOW);
+		}
+		ledOn = !ledOn;
+	}, 500);
 });
