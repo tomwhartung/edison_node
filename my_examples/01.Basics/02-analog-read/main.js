26d51faf8177dd6bfbff755d12f0adecf68f5d1d
Tom W Hartung <tomwhartung@gmail.com>
2016-05-10 06:15:19 +0000

    Put read and write operations in a function called by setInterval so it is called repeatedly and indefinitely.

--- my_examples/01.Basics/02-analog-read/main.js
+++ my_examples/01.Basics/02-analog-read/main.js
@@ -20,5 +20,11 @@ var mraa = require('mraa'); //require mraa
 console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
 
 var analogPin0 = new mraa.Aio(0); //setup access analog input Analog pin #0 (A0)
-var analogValue = analogPin0.read(); //read the value of the analog pin
-console.log(analogValue); //write the value of the analog pin to the console
+
+function displayCurrentValue() {
+   var analogValue = analogPin0.read();  // read the value of the analog pin
+   console.log(analogValue);             // write the value of the analog pin to the console
+}
+
+var delayBetweenReads = 1000;
+setInterval( displayCurrentValue, delayBetweenReads );
