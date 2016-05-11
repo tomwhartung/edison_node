31d9856e5086373caf525adbbef1a9003b8f5107
Tom W Hartung <tomwhartung@gmail.com>
2016-05-11 02:40:33 +0000

    Adding 08-buttonTurnsOnLed, which leaves the led on as long as the button is being pressed, and turns it off otherwise.  This one works better than the one that toggles the state (or at least is supposed to do so) when the button is pressed.

---
+++ my_examples/01.Basics/08-buttonTurnsOnLed/main.js
@@ -0,0 +1,69 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+
+/*
+ * A simple node.js application intended to read data from Digital pins on the Intel based 
+ *   development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
+ *
+ * MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+ * Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with 
+ *   port nanmes/numbering that match boards & with bindings to javascript & python.
+ *
+ * Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
+ * Using a ssh client: 
+ * 1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+ * 2. opkg update
+ * 3. opkg upgrade
+ *
+ * Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
+ */
+
+var mraa = require('mraa'); //require mraa
+
+var pin8Button = new mraa.Gpio( 8 ); // setup digital read on Digital pin #6 (D6)
+var pin4Led = new mraa.Gpio( 4 );    // setup digital read on Digital pin #4 (D4)
+
+var OFF = 0;
+var ON = 1;
+
+var pin4State = OFF;
+var loopMs = 333;
+
+/*
+ * Functions - useful subroutines
+ */
+function turnOffPin4() {
+   console.log( 'Turning off pin4State ' );
+   pin4Led.write( OFF );
+}
+function turnOnPin4() {
+   console.log( 'Turning on pin4State ' );
+   pin4Led.write( ON );
+}
+
+/*
+ * Functions - setup and loop
+ */
+function setup() {
+   console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+   pin8Button.dir( mraa.DIR_IN );   // button: set the gpio direction to input
+   pin4Led.dir( mraa.DIR_OUT );     // led: set the gpio direction to output
+}
+
+function loop() {
+   var pin8Value =  pin8Button.read();     // read the digital value of the pin
+   console.log( 'Gpio is ' + pin8Value );  // write the read value out to the console
+   if ( pin8Value == 1 ) {
+      turnOnPin4();
+   } else {
+      turnOffPin4();
+   }
+   setTimeout( loop, loopMs );
+}
+
+/*
+ * Mainline: call setup and loop
+ */
+setup();
+loop();
+
