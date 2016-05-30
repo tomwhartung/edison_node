6ebcc85aafd04289896f177d85cbf331df6816cb
Tom W Hartung <tomwhartung@gmail.com>
2016-05-30 00:24:58 +0000

    Formatted the comments in the header the way I like to (so you can easily tell they are comments and not code) and updated the ipAddress with the static IP for humphrey.

--- my_examples/21-simple_web_server/main.js
+++ my_examples/21-simple_web_server/main.js
@@ -2,25 +2,21 @@
 /*jshint unused:true */
 /*global */
 /*
-A simple node.js application intended to read data from Analog pins on the Intel based development boards 
-such as the Intel(R) Galileo and Edison with Arduino breakout board, and display it in a browser running on the client.
-
-This demonstrates use of http.createServer, and fs.
-
-MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
-Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
-  
-Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
-Using a ssh client: 
-1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
-2. opkg update
-3. opkg upgrade
-
-Article: https://software.intel.com/en-us/xdk-sample-creating-a-web-server
-*/
+ * A simple node.js application intended to read data from Analog pins on the Intel based development boards 
+ * such as the Intel(R) Galileo and Edison with Arduino breakout board, and display it in a browser running on the client.
+ * This demonstrates use of http.createServer, and fs.
+ * MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+ * Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+ * Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
+ * Using a ssh client: 
+ * 1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+ * 2. opkg update
+ * 3. opkg upgrade
+ * Article: https://software.intel.com/en-us/xdk-sample-creating-a-web-server
+ */
 
 // Set this to the ip address of your board (not 127.0.0.1)
-var ipAddress = '192.168.2.15'; 
+var ipAddress = '10.0.0.115'; 
 
 var mraa = require('mraa'); //require mraa
 console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
