9c85a387b85fffb87b604d9801a16957d4fe4fb4
Tom W Hartung <tomwhartung@gmail.com>
2016-05-30 22:47:55 -0600

    First pass at code to turn off the lcd; need to commit and test.

--- my_examples/30.lcd/301-turn_it_off/main.js
+++ my_examples/30.lcd/301-turn_it_off/main.js
@@ -29,56 +29,17 @@ else {
     console.log('meaa version(' + version + ') is old - this code may not work');
 }
 
-if (useUpmVersion) {
-    useUpm();
-}
-else {
-    useLcd();
-}
+useLcd();
 
 /**
  * Rotate through a color pallette and display the
  * color of the background as text
  */
-function rotateColors(display) {
+function turnItOff(display) {
     var red = 0;
     var green = 0;
     var blue = 0;
     display.setColor(red, green, blue);
-    setInterval(function() {
-        blue += 64;
-        if (blue > 255) {
-            blue = 0;
-            green += 64;
-            if (green > 255) {
-                green = 0;
-                red += 64;
-                if (red > 255) {
-                    red = 0;
-                }
-            }
-        }
-        display.setColor(red, green, blue);
-        display.setCursor(0,0);
-        display.write('red=' + red + ' grn=' + green + '  ');
-        display.setCursor(1,0);
-        display.write('blue=' + blue + '   ');  // extra padding clears out previous text
-    }, 1000);
-}
-
-/**
- * Use the upm library to drive the two line display
- *
- * Note that this does not use the "lcd.js" code at all
- */
-function useUpm() {
-    var lcd = require('jsupm_i2clcd');
-    var display = new lcd.Jhd1313m1(0, 0x3E, 0x62);
-    display.setCursor(1, 1);
-    display.write('hi there');
-    display.setCursor(0,0);
-    display.write('more text');
-    rotateColors(display);
 }
 
 /**
@@ -97,11 +58,11 @@ function useLcd() {
     display.write('more text');
     display.waitForQuiescent()
     .then(function() {
-        rotateColors(display);
+        turnItOff(display);
     })
     .fail(function(err) {
         console.log(err);
         display.clearError();
-        rotateColors(display);
+        turnItOff(display);
     });
 }
