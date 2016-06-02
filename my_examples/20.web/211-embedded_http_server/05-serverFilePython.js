c9d1e634cbbb57c5e1822b18d09d79dea12ba1ac
Tom W Hartung <tomwhartung@gmail.com>
2016-06-02 17:08:49 +0000

    Latest code changes made trying to get this to work.

--- my_examples/20.web/211-embedded_http_server/05-serverFilePython.js
+++ my_examples/20.web/211-embedded_http_server/05-serverFilePython.js
@@ -6,17 +6,24 @@ var http = require( 'http' );
 // var fs = require( 'fs' );
 // var helloPythonPage = fs.readFileSync('helloPython.py');
 var sys = require( 'sys' );
-var exec = require( 'child_process' ).exec;
+// var exec = require( 'child_process' ).exec;
+var execFile = require( 'child_process' ).execFile;
 
 function helloPythonStdout( error, stdout, stderr ) {
 // sys.puts(stdout)
-   return stdout;
+   console.log( 'error: ' + error );
+   console.log( 'stdout: ' + stdout );
+   console.log( 'stderr: ' + stderr );
+   return 'Can I return stdout from this function?';
+// return stdout;
 }
-exec( "helloPython.py", helloPythonStdout);
+var args = [];
+var options = '';
+execFile( "helloPython.py", args, options, helloPythonStdout );
 
 var server = http.createServer( function (req, res) {
-// var helloPythonMarkup = helloPythonStdout();
-   var helloPythonMarkup = 'helloPythonStdout';
+   var helloPythonMarkup = helloPythonStdout();
+// var helloPythonMarkup = 'helloPythonStdout';
 	res.writeHead( 200, {'Content-type': 'text/html'} );
 	res.write( helloPythonMarkup );
 	res.end();
