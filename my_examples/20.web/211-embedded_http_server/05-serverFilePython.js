990bcb0b89aa8760bbed5a670d4332154878af68
Tom W Hartung <tomwhartung@gmail.com>
2016-06-02 19:21:12 +0000

    Added comments (function header) explaining that this program does not work (probably due to the old version of node running on the Edison) along with a reference to the article on stackoverflow where I got the idea for the code and noting that we really have no need to get this to work right now.

--- my_examples/20.web/211-embedded_http_server/05-serverFilePython.js
+++ my_examples/20.web/211-embedded_http_server/05-serverFilePython.js
@@ -9,6 +9,22 @@ var sys = require( 'sys' );
 // var exec = require( 'child_process' ).exec;
 var execFile = require( 'child_process' ).execFile;
 
+/**
+ * This is not working, probably due to the version of node on the Edison being fairly old (v0.10.38).
+ * Moreover I am just experimenting and really have no real need to get this to work right now.
+ *
+ * This is the output I am seeing:
+ *   $ node 05-serverFilePython.js
+ *   starting server at port: 3000
+ *   error: undefined
+ *   stdout: undefined
+ *   stderr: undefined
+ *
+ * References:
+ *   http://stackoverflow.com/questions/20643470/execute-a-command-line-binary-with-node-js
+ * The article above refers to documentation for version v6.2.0 of node:
+ *   https://nodejs.org/api/child_process.html
+ */
 function helloPythonStdout( error, stdout, stderr ) {
 // sys.puts(stdout)
    console.log( 'error: ' + error );
