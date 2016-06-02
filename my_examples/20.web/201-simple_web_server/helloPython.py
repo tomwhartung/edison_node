ada1d8c9a9be7289a9c5226552bc6fd991eeb84e
Tom W Hartung <tomwhartung@gmail.com>
2016-06-01 22:42:01 -0600

    Adding a simple helloPython.py file, to see if we can run python from this server.  Frankly I doubt this will work but hey it is worth a try, so why not, eh?

---
+++ my_examples/20.web/201-simple_web_server/helloPython.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+# hello.py: From p. 135 of the book Python in Easy Steps
+# ------------------------------------------------------
+#
+print( '<!DOCTYPE HTML>' )
+print( '<html lang="en">' )
+print( '<head>' )
+print( '<meta charset="UTF=8">' )
+print( '<title>hello.py</title>' )
+print( '</head>' )
+print( '<body>' )
+print( '<h1>Hello from hello.py</h1>' )
+print( '</body>' )
+print( '</html>' )
+
