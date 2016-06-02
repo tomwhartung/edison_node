0a3fdc36f57dbe41a34f3dc90aa82e9d19d3a1f8
Tom W Hartung <tomwhartung@gmail.com>
2016-06-02 05:15:27 +0000

    Adding the simplest version of our server program with a name that makes sense and will serve (pun not intended) us well in the long run.

---
+++ my_examples/20.web/211-embedded_http_server/01-serverHello.js
@@ -0,0 +1,17 @@
+/**
+ * Web server inpsired by Node.js for Embedded Systems by Patrick Mulder
+ * This version is the simplest possible server, our good friend "Hello..."
+ */
+var http = require( 'http' );
+
+var server = http.createServer( function (req, res) {
+	res.writeHead( 200, {'Content-type': 'text/html'} );
+	res.write( 'Hello web surfer!\n' );
+	res.end();
+});
+//
+// start server at a port
+//
+var port = 3000;
+server.listen( port );
+console.log( 'starting server at port: ' + port );
