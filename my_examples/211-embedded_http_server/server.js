43e88970f7fdcd9eec99cba6ad0d2bd7b158d3a4
Tom W Hartung <tomwhartung@gmail.com>
2016-06-01 22:06:26 -0600

    First pass at creating a web server, server.js .

---
+++ my_examples/211-embedded_http_server/server.js
@@ -0,0 +1,17 @@
+/**
+ * Web server inpsired by Node.js for Embedded Systems by Patrick Mulder
+ *
+ */
+var http = require( 'http' );
+
+var server = http.createServer( function (req, res) {
+	res.writeHead( 200, {'Content-type': 'text/html'} );
+	res.write( 'event\n' );
+	res.end();
+});
+//
+// start server at a port
+//
+var port = 3000;
+server.listen( port );
+console.log( 'starting server at port: ' + port );
