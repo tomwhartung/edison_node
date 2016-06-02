abc4bb6ae83031bb834dbe36111f1db5d6cc6271
Tom W Hartung <tomwhartung@gmail.com>
2016-06-02 04:24:43 +0000

    Updated server.js to write some markup, instead of just some bare text.

--- my_examples/20.web/211-embedded_http_server/server.js
+++ my_examples/20.web/211-embedded_http_server/server.js
@@ -5,10 +5,24 @@
 var http = require( 'http' );
 
 var server = http.createServer( function (req, res) {
+   var markup = getSomeMarkup();
 	res.writeHead( 200, {'Content-type': 'text/html'} );
-	res.write( 'Hello web surfer!\n' );
+//	res.write( 'Hello web surfer!\n' );
+	res.write( markup );
 	res.end();
 });
+/**
+ * Try returning some markup instead of just straight text
+ */
+function getSomeMarkup() {
+   var markup = '';
+   markup += '<body>';
+   markup += '<h1>Heading level 1</h1>';
+   markup += '<p>Some text in a paragraph.';
+   markup += '</p>';
+   markup += '</body>';
+   return markup;
+}
 //
 // start server at a port
 //
